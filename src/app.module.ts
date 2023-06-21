457843912af3c2ea8f6867f2f4d52c6ece81b82b
feelonewong <feelonewong@gmail.com>
2023-06-21 10:39:31 +0800

    update:8-7新建表

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RangeModule } from './range/range.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Profile } from './user/entities/profile.entity';
 @Module({
-  imports: [RangeModule],
+  imports: [
+    RangeModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // 数据库用户名
+      password: '123456', // 数据库密码
+      database: 'nest-blog', // 数据库名
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // 实体类文件所在位置
+      // entities: [User, Profile],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
